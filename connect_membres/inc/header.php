2cb1ef43c15d84669bc1e5eb93437455d51ad928
franck <bigmoletos@yopmail.com>
2018-02-04 10:55:09 +0100

    first commit

---
+++ connect_membres/inc/header.php
@@ -0,0 +1,67 @@
+<?php 
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * https://getbootstrap.com/docs/3.3/examples/starter-template/
+ */
+ ?>
+
+
+  
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="page header">
+    <meta name="author" content="fd">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>PAGE HEADER</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/app.css" rel="stylesheet">
+
+    
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse ">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Projet connexion membres</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="register.php">S'incrire</a></li>
+            <li><a href="login.php">se connecter</a></li>
+            <!--<li><a href="#contact">Contact</a></li>-->
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+
+
+  
+  </body>
+</html>
